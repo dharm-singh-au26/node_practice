f24cb3650468f5d8dfee64c963d287a7589b88f1
dharm <dharmjat45@gmail.com>
2022-02-09 22:15:16 +0530

    done first function with export module

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 
 const utility = require("./utility");
+
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("hello world");
+    let sum = utility(2, 5);
+    // console.log(sum);
+    res.end("hello world" + "sum" + sum);
   })
   .listen(3000);
