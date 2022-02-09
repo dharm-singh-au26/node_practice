1dcc9eefc2f4eedf62b62a78f598ee4e3ec7f0d4
dharm <dharmjat45@gmail.com>
2022-02-09 23:26:55 +0530

    created express server

--- Express/app.js
+++ Express/app.js
@@ -1,3 +1,4 @@
+/*
 const http = require("http");
 
 const utility = require("./utility");
@@ -10,3 +11,20 @@ http
     res.end("hello world" + "sum" + sum);
   })
   .listen(3000);
+  */
+
+// using express create server
+
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.get("/home", (req, res) => {
+  res.send("Hello we are at home");
+});
+
+app.listen(port, () => console.log(`server running at ${port}`));
